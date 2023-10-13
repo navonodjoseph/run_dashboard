f222b685cc4847cdbe783a32f9bd1b8034b07760
navonodjoseph <joepeterdonovan@gmail.com>
2023-10-13 13:12:25 -0400

    configuring react app

---
+++ run_dash/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello, this is my run dashboard</p>
+    </div>
+  );
+}
+
+export default App;
